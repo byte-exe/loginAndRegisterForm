0c161e2ca04aa4a2a4429569be3bf6c008b0debc
Vyneee <58006737+byte-exe@users.noreply.github.com>
2020-12-15 01:01:04 +0700

    First commit

---
+++ src/apphospital/AppHospital.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apphospital;
+
+/**
+ *
+ * @author Zynee
+ */
+public class AppHospital {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
